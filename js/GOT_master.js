3d271af8775b4f922ac035c6ac0105f1b2022b05
Obasa <i_obasa@tss-hr420-dm15.local>
2019-07-09 17:40:02 -0400

    lightbox added

---
+++ js/GOT_master.js
@@ -0,0 +1,34 @@
+(() => {
+  console.log('fired!');
+
+  //variable stack -> get the shields / sigils first
+  const sigils = document.querySelectorAll('.sigilContainer'),
+        lightBox = document.querySelector('.lightbox'),
+        closeButton = document.querySelector('.close-lightbox'),
+        houseVideo = document.querySelector('.house-video');
+
+  function popLightBox() {
+    // debug this so far and make sure the event handline works
+    // debugger;
+
+    //make lightbox show up
+    lightBox.classList.add('show-lightbox');
+
+    houseVideo.play();
+  }
+
+  function closeLightBox(event) {
+    event.preventDefault();
+
+    // make the lightbox close
+    lightBox.classList.remove('show-lightbox');
+    houseVideo.currentTime = 0; // rewind the video
+    houseVideo.pause();
+  }
+
+  sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+  closeButton.addEventListener("click", closeLightBox);
+
+  houseVideo.addEventListener('ended', closeLightBox);
+
+})();
